50b9dd86f5b8b6a4308e7e0ae2899a1a7d315c76
bunasai4 <bunasai4@gmail.com>
2025-03-31 13:50:19 +0000

    idk

--- hosting/src/App.jsx
+++ hosting/src/App.jsx
@@ -5,6 +5,9 @@ import { auth } from "./config/firebase";
 import routes from "./config/routes";
 import Center from "./components/utils/Center";
 import AuthChecker from "./components/auth/AuthChecker";
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -46,10 +49,38 @@ function App() {
               }
             />
           ))}
+          <Route path="/basic" element={
+            <>
+              <div>
+                <a href="https://vite.dev" target="_blank" rel="noreferrer">
+                  <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank" rel="noreferrer">
+                  <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+              </div>
+              <h1>Vite + React</h1>
+              <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                  count is {count}
+                </button>
+                <p>
+                  Edit <code>src/App.jsx</code> and save to test HMR
+                </p>
+              </div>
+              <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+              </p>
+            </>
+          } />
         </Routes>
       </BrowserRouter>
     </div>
   );
+}
+
+function setCount(countSetter){
+  countSetter(old=> old +1);
 }
 
 export default App;
